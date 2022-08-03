5f06680aea142113cbddd60edf0280ff8a61fb6f
Guilherme Santos <guuilhermessantos@gmail.com>
2022-08-03 17:29:01 -0300

    Finalizando deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import cors from 'cors'
 
 const app = express() 
 console.log(process.env.PORT)
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
 
 app.use(cors())
 app.use(express.json());
