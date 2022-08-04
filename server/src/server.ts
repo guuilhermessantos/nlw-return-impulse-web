d162b77bfab2cabffa6856a434642bc4c5b1feb0
Guilherme Santos <guuilhermessantos@gmail.com>
2022-08-04 11:41:33 -0300

    finalizando deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,16 +3,15 @@ import nodemailer from 'nodemailer'
 import { routes } from './routes';
 import cors from 'cors'
 
-
 const app = express() 
 
 app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-console.log(process.env.PORT)
+console.log(process.env.PORT || 3333)
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`HTTP server running! ${process.env.PORT} 🎢`)
+app.listen(process.env.PORT || 3333, () => {
+    console.log(`HTTP server running! ${process.env.PORT || 3333} 🎢`)
 })
